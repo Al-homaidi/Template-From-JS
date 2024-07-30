08ede162c14b86b8c296ff823db9babdfa7f6374
Al-Homede-1 <155636135+Al-Homede-1@users.noreply.github.com>
2024-07-30 23:00:26 +0300

    adding: all section after about us

--- js/master.js
+++ js/master.js
@@ -14,7 +14,10 @@ if (localStoragebackground !== null) {
     document.querySelector(".buttons .yes").classList.add("active");
   } else document.querySelector(".buttons .No").classList.add("active");
 }
-let buttonsrandombackground = document.querySelectorAll(".buttons span");
+
+let buttonsrandombackground = document.querySelectorAll(
+  ".option-box .buttons span"
+);
 buttonsrandombackground.forEach((span) => {
   span.addEventListener("click", function (e) {
     buttonsrandombackground.forEach((ele) => {
@@ -29,6 +32,46 @@ buttonsrandombackground.forEach((span) => {
       window.localStorage.setItem("data_background", false);
       clearInterval(backgroundInterval);
       backgroundOption = false;
+    }
+  });
+});
+
+let localStoragedata_displaynav =
+  window.localStorage.getItem("data_displaynav");
+if (localStoragedata_displaynav !== null) {
+  if (localStoragedata_displaynav === "true") {
+    document.querySelector(".nav-bullets").style.display = "block";
+  } else {
+    document.querySelector(".nav-bullets").style.display = "none";
+  }
+  document.querySelectorAll(".testing-option .buttons span").forEach((ele) => {
+    ele.classList.remove("active");
+  });
+  if (localStoragedata_displaynav === "true") {
+    document
+      .querySelector(".testing-option .buttons .yes")
+      .classList.add("active");
+  } else
+    document
+      .querySelector(".testing-option .buttons .No")
+      .classList.add("active");
+}
+
+let buttonsnavbullets = document.querySelectorAll(
+  ".testing-option .buttons span"
+);
+buttonsnavbullets.forEach((span) => {
+  span.addEventListener("click", function (e) {
+    buttonsnavbullets.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    if (e.target.dataset.displaynav === "yes") {
+      window.localStorage.setItem("data_displaynav", true);
+      document.querySelector(".nav-bullets").style.display = "block";
+    } else {
+      window.localStorage.setItem("data_displaynav", false);
+      document.querySelector(".nav-bullets").style.display = "none";
     }
   });
 });
@@ -57,7 +100,7 @@ function randomizImge() {
     backgroundInterval = setInterval(() => {
       let roundmnumber = Math.floor(Math.random() * imges.length);
       img.style.backgroundImage = `url(${imges[roundmnumber]})`;
-    }, 1000);
+    }, 10000);
   }
 }
 randomizImge();
@@ -98,5 +141,64 @@ colorli.forEach((li) => {
       ele.classList.remove("active");
     });
     this.classList.add("active");
+  });
+});
+
+let ourskils = document.querySelector(".Our-Skills");
+
+window.onscroll = function () {
+  let ourskilsscroltop = ourskils.offsetTop;
+  let ourskilsouterheight = ourskils.offsetHeight;
+  let windowheight = this.innerHeight;
+  let windowScrollTop = this.pageYOffset;
+  if (windowScrollTop > ourskilsscroltop + ourskilsouterheight - windowheight) {
+    let allrskils = document.querySelectorAll(
+      ".Our-Skills .skill-progress span"
+    );
+    allrskils.forEach((skil) => {
+      skil.style.width = skil.dataset.progress;
+    });
+  }
+};
+let imagesbox = document.querySelectorAll(".images-box img");
+imagesbox.forEach((img) => {
+  img.addEventListener("click", function () {
+    let overlay = document.createElement("div");
+    overlay.classList.add("popup-overlay");
+    document.body.appendChild(overlay);
+
+    let popup = document.createElement("popup");
+    popup.classList.add("popup");
+    document.body.appendChild(popup);
+
+    if (img.alt) {
+      let altimg = document.createElement("h3");
+      altimg.innerHTML = img.alt;
+      popup.appendChild(altimg);
+    }
+
+    let imgpopup = document.createElement("img");
+    imgpopup.src = img.src;
+    imgpopup.classList.add("imgpopup");
+    popup.appendChild(imgpopup);
+
+    let buttoncancel = document.createElement("span");
+    buttoncancel.classList.add("buttoncancel");
+    buttoncancel.innerHTML = "X";
+    popup.appendChild(buttoncancel);
+
+    buttoncancel.addEventListener("click", function () {
+      overlay.style.display = "none";
+      popup.style.display = "none";
+    });
+  });
+});
+
+let navbullets = document.querySelectorAll(".nav-bullets .bullet");
+navbullets.forEach((ele) => {
+  ele.addEventListener("click", function () {
+    document.querySelector(ele.dataset.section).scrollIntoView({
+      behavior: "smooth",
+    });
   });
 });
