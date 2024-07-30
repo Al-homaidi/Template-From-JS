e2fd1f489b35dec74839909d4f39e7eb7f4e4fdc
Al-Homede-1 <155636135+Al-Homede-1@users.noreply.github.com>
2024-07-30 09:32:24 +0300

    adding: section about us

--- js/master.js
+++ js/master.js
@@ -1,3 +1,38 @@
+let backgroundOption = true;
+let backgroundInterval;
+let localStoragebackground = window.localStorage.getItem("data_background");
+if (localStoragebackground !== null) {
+  if (localStoragebackground === "true") {
+    backgroundOption = true;
+  } else {
+    backgroundOption = false;
+  }
+  document.querySelectorAll(".buttons span").forEach((ele) => {
+    ele.classList.remove("active");
+  });
+  if (localStoragebackground === "true") {
+    document.querySelector(".buttons .yes").classList.add("active");
+  } else document.querySelector(".buttons .No").classList.add("active");
+}
+let buttonsrandombackground = document.querySelectorAll(".buttons span");
+buttonsrandombackground.forEach((span) => {
+  span.addEventListener("click", function (e) {
+    buttonsrandombackground.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    if (e.target.dataset.background === "yes") {
+      window.localStorage.setItem("data_background", true);
+      backgroundOption = true;
+      randomizImge();
+    } else {
+      window.localStorage.setItem("data_background", false);
+      clearInterval(backgroundInterval);
+      backgroundOption = false;
+    }
+  });
+});
+
 let links = document.querySelectorAll("ul li a");
 links.forEach((ele) => {
   ele.onclick = function () {
@@ -7,6 +42,7 @@ links.forEach((ele) => {
     this.classList.add("active");
   };
 });
+
 const imges = [
   "../imgs/01.jpg",
   "../imgs/02.jpg",
@@ -14,13 +50,17 @@ const imges = [
   "../imgs/04.jpg",
   "../imgs/05.jpg",
 ];
-let img = document.querySelector(".landing-page");
-let landingpagefunction = function () {
-  let roundmnumber = Math.floor(Math.random() * imges.length);
-  img.style.backgroundImage = `url(${imges[roundmnumber]})`;
-};
+function randomizImge() {
+  if (backgroundOption === true) {
+    let img = document.querySelector(".landing-page");
 
-setInterval(landingpagefunction, 10000);
+    backgroundInterval = setInterval(() => {
+      let roundmnumber = Math.floor(Math.random() * imges.length);
+      img.style.backgroundImage = `url(${imges[roundmnumber]})`;
+    }, 1000);
+  }
+}
+randomizImge();
 
 let Settingsbox = document.querySelector(".Settings-box");
 let gearIcon = document.querySelector(".fa-gear");
@@ -34,3 +74,29 @@ togglesettings.onclick = function () {
   }
   gearIcon.classList.toggle("fa-spin");
 };
+
+const colorli = document.querySelectorAll(".colors-list li");
+let localStoragecolor = localStorage.getItem("main-color");
+if (localStoragecolor !== null) {
+  document.documentElement.style.setProperty("--main-color", localStoragecolor);
+  colorli.forEach((ele) => {
+    ele.classList.remove("active");
+    if (ele.dataset.color === localStoragecolor) {
+      ele.classList.add("active");
+    }
+  });
+}
+
+colorli.forEach((li) => {
+  li.addEventListener("click", function (e) {
+    window.localStorage.setItem("main-color", e.target.dataset.color);
+    document.documentElement.style.setProperty(
+      "--main-color",
+      e.target.dataset.color
+    );
+    colorli.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    this.classList.add("active");
+  });
+});
